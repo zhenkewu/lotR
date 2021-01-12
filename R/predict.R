80010a56f2b422bd3ca1f61f31ef61fd0343412a
zhenkewu <zhenkewu@gmail.com>
2021-01-11 22:26:03 -0500

    add predict.blca

--- R/predict.R
+++ R/predict.R
@@ -1,4 +1,5 @@
-#' plots the groups discovered by lcm_tree on the tree for observations.
+#' approximate predictive class probabilities for a new observation in
+#' a leaf
 #'
 #' @param object An `lcm_tree` class object; Output from `lcm_tree()`
 #' @param dat_pred a binary matrix with rows for observations and columns
@@ -16,7 +17,6 @@
 #' an observation in each class.
 #'
 #' @importFrom matrixStats logSumExp
-#' @rdname predict.lcm_tree
 #' @export
 predict.lcm_tree <- function(object,
                              dat_pred=NULL,leaf_ids=NULL,collapsed=TRUE,
@@ -70,3 +70,64 @@ predict.lcm_tree <- function(object,
 # aa = predict.lcm_tree(mod0,simu_dat_pred,simu_pred$curr_leaves,FALSE)
 # bb = predict.lcm_tree(mod0,simu_dat_pred,simu_pred$curr_leaves,TRUE)
 # table(apply(aa,1,which.max),apply(bb,1,which.max))
+
+
+#' approximate predictive class probabilities for a new observation
+#'
+#' @param object An `lcm_tree` class object; Output from `BayesLCA::blca()`
+#' @param dat_pred a binary matrix with rows for observations and columns
+#' for features (of the same dimension as in the training data)
+#' @param ... Other parameters
+#'
+#' @return a matrix with the same number of rows as `dat_pred` and
+#' `K` columns (`K` is the number of latent classes);
+#' each row correspond to a vector of predicted probabilities of
+#' an observation in each class.
+#'
+#' @importFrom matrixStats logSumExp
+#' @export
+predict.blca <- function(object,
+                         dat_pred=NULL,
+                         ...){
+  # # ## test
+  # object <- res
+  # dat_pred <- simu_dat_pred
+  # # leaf_ids <- simu_pred$curr_leaves
+  # # ## end of test
+  N_pred <- nrow(dat_pred)
+  K <- nrow(object$itemprob)
+
+  postprob <- function(pi_vec,theta_mat,Y){
+    K <- length(pi_vec)
+    res <- rep(NA,K)
+    for (k in 1:K){
+      res[k] <- sum(Y*log(theta_mat[,k])+(1-Y)*log(1-theta_mat[,k]))+log(pi_vec[k])
+    }
+    exp(res - logSumExp(res))
+  }
+
+  probpred_mat <- matrix(NA,nrow=N_pred,ncol=K)
+  curr_pi_vec  <- object$classprob
+  for (i in 1:nrow(dat_pred)){
+    probpred_mat[i,] <- postprob(curr_pi_vec,t(object$itemprob),dat_pred[i,])
+  }
+  probpred_mat
+}
+
+# x <- mod0
+# dat_pred <- simu_dat_pred
+# leaf_ids <- simu_pred$curr_leaves
+#
+# aa = predict.lcm_tree(mod0,simu_dat_pred,simu_pred$curr_leaves,FALSE)
+# bb = predict.lcm_tree(mod0,simu_dat_pred,simu_pred$curr_leaves,TRUE)
+# table(apply(aa,1,which.max),apply(bb,1,which.max))
+
+
+# x <- mod0
+# dat_pred <- simu_dat_pred
+# leaf_ids <- simu_pred$curr_leaves
+#
+# aa = predict.lcm_tree(mod0,simu_dat_pred,simu_pred$curr_leaves,FALSE)
+# bb = predict.lcm_tree(mod0,simu_dat_pred,simu_pred$curr_leaves,TRUE)
+# table(apply(aa,1,which.max),apply(bb,1,which.max))
+
